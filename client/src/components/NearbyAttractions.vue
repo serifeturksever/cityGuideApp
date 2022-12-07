081cda5f91e50a01119e2c85f7f880cfb04c722e
serifeturksever <62253012+serifeturksever@users.noreply.github.com>
2022-12-07 11:13:24 +0300

    .

--- client/src/components/NearbyAttractions.vue
+++ client/src/components/NearbyAttractions.vue
@@ -1,64 +1,58 @@
 <template>
     <div id="container">
-        <div id="filter-attraction">
-            <form>
-                <div>
+        <div class="modal"></div>
+        <div id="filter-result">
+            <div id="filter-attraction">
+                <form>
                     <div>
-                        <div id="lat-lng">
-                            <input type="text" placeholder="Enter your address" v-model="coordinates" />
-                            <!-- <button id="find-me" @click="locatorButtonPressed">find me</button> find my location -->
-                            <a id="fancy-button" href="#"><span id="btn-span" @click="locatorButtonPressed">Find me!</span></a>
-                        </div>
-                    </div>
-                    <div class="filter-container">
-                        <div class="sub-filter-container">
-                            <div class="attraction-type">
-                                <select v-model="type">
-                                    <option disabled value="">Attraction Type</option>
-                                    <option value="all">See All Attractions</option>
-                                    <option value="restaurant">Restaurant</option>
-                                    <option selected value="travel_agency">Travel Agency</option>
-                                    <option value="hospital">Hospital</option>
-                                    <option value="hair_care">Hair Care</option>
-                                    <option value="stadium">Stadium</option>
-                                    <option value="police">Police</option>
-                                    <option value="pharmacy">Pharmacy</option>
-                                    <option value="night_club">Night Club</option>
-                                    <option value="museum">Museum</option>
-                                    <option value="mosque">Mosque</option>
-                                    <option value="amusement_park">Amusement Park</option>
-                                    <option value="art_gallery">Art Gallery</option>
-                                </select>
-                            </div>
-                            <div class="radius">
-                                <select v-model="radius">
-                                    <option disabled value="">Radius</option>
-                                    <option value="1">1 KM</option>
-                                    <option value="5">5 KM</option>
-                                    <option value="10">10 KM</option>
-                                    <option value="15">15 KM</option>
-                                    <option value="20">20 KM</option>
-                                    <option value="25">25 KM</option>
-                                    <option value="50">50 KM</option>
-                                </select>
+                        <div>
+                            <div id="lat-lng">
+                                <input type="text" placeholder="Enter your address" v-model="coordinates" />
+                                <a id="fancy-button"><span id="btn-span" style="cursor: pointer;"
+                                        @click="locatorButtonPressed">Find me!</span></a>
                             </div>
                         </div>
+                        <div class="filter-container">
+                            <div class="sub-filter-container">
+                                <div class="attraction-type">
+                                    <select v-model="type">
+                                        <option disabled value="">Attraction Type</option>
+                                        <option v-for="attraction in attractions" :value="attraction.value">{{attraction.name}}</option>
+                                    </select>
+                                </div>
+                                <div class="radius">
+                                    <select v-model="radius">
+                                        <option disabled value="">Radius</option>
+                                        <option v-for="distance in distances" :value="distance.value">{{distance.value}}</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <a id="fancy-button" style="cursor: pointer;"><span id="btn-span"
+                                @click="findCloseBuyButtonPressed">Find Nearby Attractions</span></a>
                     </div>
-                        <a id="fancy-button" href="#"><span id="btn-span" @click="findCloseBuyButtonPressed">Find Nearby Attractions</span></a>
-                    <!-- <button id="find-nearby-attractions-btn" @click="findCloseBuyButtonPressed">Find Nearby Attractions</button> -->
+                </form>
+            </div>
+            <div id="close-places-list" style="height:200px;
+                overflow:scroll;
+                margin-top:32px;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+                display:none">
+                <div v-for="place in places" :key="place.id">
+                    <div class="list-el">
+                        <div class="left">
+                            <h4>{{ place.name }}</h4>
+                            <div style="font-size:16px">{{ place.vicinity }}</div>
+                        </div>
+                        <div class="star">star: {{ (place.rating ? place.rating : "no info") }}</div>
+                    </div>
+                    <hr>
                 </div>
-                <!-- <div id="close-places-list" class="ui segment" style="max-height:500px;overflow:scroll;display:none">
-                    <div class="ui divided items">
-                        <div class="item" v-for="place in places" :key="place.id">
-                            <div class="content">
-                                <div class="header">{{ place.name }}</div>
-                                <div class="meta">{{ place.vicinity }}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-            </form>
+            </div>
         </div>
+
         <div id="map" ref="map" style="display:none"></div>
         <div id="map-preinformation" style="display:flex;align-items:center;justify-content:center;font-size:24px">
             <span class="span-info">Please locate yourself, choose attraction type and radius 🥳🎉</span>
@@ -75,7 +69,31 @@ export default {
             lng: 27.1428,// 0,
             type: "",
             radius: "",
-            places: []
+            places: [],
+            attractions: [
+                {value: "all",name: "See All Attractions"},
+                {value: "restaurant",name: "Restaurant"},
+                {value: "travel_agency",name: "Travel Agency"},
+                {value: "hospital",name: "Hospital"},
+                {value: "hair_care",name: "Hair Care"},
+                {value: "stadium",name: "Stadium"},
+                {value: "police",name: "Police"},
+                {value: "pharmacy",name: "Pharmacy"},
+                {value: "night_club",name: "Night Club"},
+                {value: "museum",name: "Museum"},
+                {value: "mosque",name: "Mosque"},
+                {value: "amusement_park",name: "Amusement Park"},
+                {value: "art_gallery",name: "Art Gallery"}
+            ],
+            distances: [
+                {value: 1},
+                {value: 5},
+                {value: 10},
+                {value: 15},
+                {value: 20},
+                {value: 25},
+                {value: 50}
+            ]
         };
     },
     computed: {
@@ -95,7 +113,6 @@ export default {
                 }
             );
         },
-        // 46.288896, -79.44192
         findCloseBuyButtonPressed() {
             let type_str = this.type == 'all' ? '' : `&type=${this.type}`
             const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.lat
@@ -104,7 +121,7 @@ export default {
             axios.get(URL).then(response => {
                 this.places = response.data.results;
                 this.addLocationsToGoogleMaps();
-                // document.querySelector("#close-places-list").style.display = "block";
+                document.querySelector("#close-places-list").style.display = "block";
                 document.querySelector("#map-preinformation").style.display = "none";
                 document.querySelector("#map").style.display = "block";
             }).catch(error => {
@@ -119,6 +136,7 @@ export default {
                 center: new google.maps.LatLng(this.lat, this.lng),
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             });
+            console.log("places: ", this.places); ""
             this.places.forEach((place) => {
                 const lat = place.geometry.location.lat;
                 const lng = place.geometry.location.lng;
@@ -139,208 +157,246 @@ export default {
 }
 </script>
 <style>
-    #container { /* height: 50vh*/
-        display: flex;
+
+* {
+    font-family: sans-serif;
+}
+#container {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    height: 50vh;
+}
+
+#filter-result {
+    width: 30%;
+    min-width: 300px;
+    height: 700px;
+    min-height: 600px;
+    display: flex;
+    flex-direction: column;
+}
+
+#filter-attraction {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    min-height: 400px;
+    max-height: 400px;
+    background-color: #FEFBF6;
+    border: 1px solid transparent;
+    border-radius: 24px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+#map,
+#map-preinformation {
+    border: 1px solid transparent;
+    border-radius: 24px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding: 8px;
+    width: 60%;
+    min-width: 580px;
+}
+
+#map {
+    height: 700px;
+    min-height: 400px;
+}
+
+form {
+    width: 80%;
+    height: 60%;
+}
+
+form>div:nth-child(1) {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+@media only screen and (max-width: 1180px) {
+
+    /* 980px idi */
+    #container {
         width: 100%;
-        justify-content:space-evenly;
-        height: 50vh;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+        overflow: hidden;
+        height: 200vh;
+    }
+
+    #filter-result,
+    #map,
+    #map-preinformation {
+        width: 90%;
+    }
+
+    #filter-result {
+        margin-top: 64px;
+        height: 30%;
+    }
+
+    #map-preinformation {
+        height: 400px;
+        margin-top: 64px;
+        margin-bottom: 32px;
+        min-width: 200px;
+    }
+
+    #map {
+        margin-top: 64px;
+        height: 800px;
+        min-width: 200px;
+        margin-bottom: 32px;
+    }
+
+    .span-info {
+        width: 80%;
+    }
+
+    form {
+        width: 90%;
+        height: 90%;
     }
 
     #filter-attraction {
         display: flex;
+        height: 400px;
         justify-content: center;
         align-items: center;
-        width: 30%;
-        min-width: 300px;
-        border: 1px solid transparent;
-        border-radius: 24px;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     }
+}
 
-    #map,
-    #map-preinformation
-    {
-        border: 1px solid transparent;
-        border-radius: 24px;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        padding: 8px;
-        width: 60%;
-        min-width: 580px;
-    }
-    #map {
-        height: 600px;
-    }    
-    form {
-        width: 80%;
-        height: 60%;
-    }
+.span-info {
+    border: 1px solid transparent;
+    border-radius: 24px;
+    background-color: #D9ECF2;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    padding: 1rem
+}
 
-    form > div {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-    }
 
-    @media only screen and (max-width: 1180px) { /* 980px idi */
-        #container {
-            width: 100%;
-            flex-direction: column;
-            justify-content: space-around;
-            align-items: center;
-            overflow: hidden;
-            height: 100vh;
-        }
-        #filter-attraction,
-        #map,
-        #map-preinformation {
-            width: 90%;
-        }
-        #filter-attraction {
-            margin-top: 64px;
-            height: 30%;
-        }
 
-        #map-preinformation {
-            height: 400px;
-            margin-top: 64px;
-            margin-bottom: 32px;
-            min-width: 200px;
-        }
+#lat-lng {
+    width: 100%;
+    height: 48px;
+    display: flex;
+    justify-content: space-between;
+}
 
-        #map {
-            margin-top: 64px;
-            height: 800px;
-            min-width: 200px;
-            margin-bottom: 32px;
-        }
+#lat-lng>input {
+    width: 60%;
+    border-radius: 8px;
+    text-align: center;
+}
 
-        .span-info {
-            width: 80%;
-        }
+#lat-lng>a {
+    width: 35%;
+    border-radius: 8px;
+    min-width: 48px;
+}
 
-        form {
-            width: 90%;
-            height: 90%;
-        }
+.sub-filter-container {
+    display: flex;
+    justify-content: space-between;
+}
 
-        #filter-attraction {
-            display: flex;
-            height: 500px;
-            justify-content: center;
-            align-items: center;
-        }
-      }
+.attraction-type {
+    width: 60%;
+    height: 48px;
+}
 
-    .span-info {
-        border: 1px solid transparent;
-        border-radius: 24px;
-        background-color: #BFD6FA;
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        padding: 1rem
-    }
+.attraction-type>select,
+.radius>select {
+    width: 100%;
+    height: 100%;
+    border-radius: 8px;
+    text-align: center;
+}
 
-    
+.radius {
+    width: 36%;
+    height: 48px;
+}
 
-    #lat-lng {
-        width: 100%;
-        height: 48px;
-        display: flex;
-        justify-content: space-between;
-    }
+#find-nearby-attractions-btn {
+    height: 48px;
+    border-radius: 8px;
+}
 
-    #lat-lng > input {
-        width: 60%;
-        border-radius: 8px;
-    }
+button {
+    border: none;
+    cursor: pointer;
+}
 
-    #lat-lng > a {
-        width: 35%;
-        border-radius: 8px;
-        min-width: 48px;
-    }
+input {
+    border: 1px solid black;
+}
 
-    .sub-filter-container {
-        display: flex;
-        justify-content: space-between;
-    }
+#fancy-button {
+    display: block;
+    width: 100%;
+    height: 48px;
+    line-height: 40px;
+    font-size: 18px;
+    font-family: sans-serif;
+    text-decoration: none;
+    color: #333;
+    border: 2px solid #333;
+    letter-spacing: 2px;
+    text-align: center;
+    position: relative;
+    transition: all .35s;
+    border-radius: 8px;
+    background-color: white;
+}
 
-    .attraction-type {
-        width: 60%;
-        height: 48px;
-    }
-    .attraction-type > select,
-    .radius > select {
-        width: 100%;
-        height: 100%;
-        border-radius: 8px;
-    }
-    .radius {
-        width: 36%;
-        height: 48px;
-    }
+#btn-span {
+    position: relative;
+    z-index: 2;
+}
 
-    #find-nearby-attractions-btn {
-        height: 48px;
-        border-radius: 8px;
-    }
-    
-    button {
-        border: none;
-        cursor: pointer;
-    }
+#fancy-button:after {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
+    background: #ff003b;
+    transition: all .35s;
+}
 
-    input {
-        border: 1px solid black;
-    }
+#fancy-button:hover {
+    color: #fff;
+}
 
-    /* button */
-/*
-    .wrapper{
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
-      */
-      #fancy-button{
-        display: block;
-        width: 100%;
-        height: 48px;
-        line-height: 40px;
-        font-size: 18px;
-        font-family: sans-serif;
-        text-decoration: none;
-        color: #333;
-        border: 2px solid #333;
-        letter-spacing: 2px;
-        text-align: center;
-        position: relative;
-        transition: all .35s;
-        border-radius: 8px;
-      }
-      
-      #btn-span{
-        position: relative;
-        z-index: 2;
-      }
-      
-      #fancy-button:after{
-        position: absolute;
-        content: "";
-        top: 0;
-        left: 0;
-        width: 0;
-        height: 100%;
-        background: #ff003b;
-        transition: all .35s;
-      }
-      
-      #fancy-button:hover{
-        color: #fff;
-      }
-      
-      #fancy-button:hover:after{
-        width: 100%;
-      }
+#fancy-button:hover:after {
+    width: 100%;
+}
+
+.list-el {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-left: 4px;
+    margin-right: 4px;
+}
+
+.left {
+    width: 75%;
+    text-align: left;
+    margin-left: 16px;
+}
+
+.star {
+    width: 25%;
+    text-align: end;
+    margin-right: 32px
+}
 </style>
